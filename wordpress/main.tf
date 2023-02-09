34dc32970b3d0bd8c834deb81020f188a511ef24
Marc Saez <91618413+marcsaez@users.noreply.github.com>
2023-02-09 16:50:13 +0100

    Update main.tf

--- wordpress/main.tf
+++ wordpress/main.tf
@@ -1,32 +1,53 @@
-#----------RDS----------
+#---------------SSH PUBLIC KEY---------------
+resource "aws_key_pair" "marc" {
+  key_name   = "${var.name}aws_key"
+  public_key = var.ssh_key
+}
+
+#---------------RDS---------------
 resource "aws_db_instance" "bbdd" {
-    identifier = "msaezbbdd"
-    db_name = "wordpress"
+    identifier = "${var.name}bbdd"
+    db_name = var.db_name
     engine = "mysql"
-    username = "admin"
-    password = "p4ssword!"
-    instance_class = "db.t2.micro"
-    allocated_storage = 20
+    username = var.db_admin
+    password = var.db_pass
+    instance_class = var.db_type
+    allocated_storage = var.db_store
     vpc_security_group_ids = [aws_security_group.bbdd_ec2_conncetion.id]
     skip_final_snapshot = true
     tags = {
-      "Name" = "mysql_marc"
+      "Name" = "${var.name}mysql"
     }
 }
 
+#---------------EC2---------------
 resource "aws_instance" "wp" {
-    ami           = "ami-0b5eea76982371e91" #Amazon Linux 2 Kernel 5.10 AMI 2.0.20221210.1 x86_64 HVM gp2
-    instance_type = "t2.micro"
-    key_name      = "aws_key_marc"
-    vpc_security_group_ids = [aws_security_group.wp.id,aws_security_group.bbdd_ec2_conncetion.id]    
+    ami           = data.aws_ami.al2.id 
+    instance_type = var.ec2_type
+    key_name      = "${var.name}aws_key"
+    vpc_security_group_ids = [aws_security_group.wp.id,aws_security_group.bbdd_ec2_conncetion.id]
+    user_data_base64 = base64encode(data.template_file.cloud-init-config.rendered)
+    provisioner "remote-exec" {
+      inline = [
+      "echo 'Waiting for cloud-init to complete...'",
+      "cloud-init status --wait > /dev/null",
+      "echo 'Completed cloud-init!'",
+      "echo 'Wordpress ready!'",
+      ]
+    } 
     tags = {
-      "Name" = "wp_marc"
+      "Name" = "${var.name}wp"
     }
+    depends_on = [
+      aws_db_instance.bbdd
+    ]
 }
 
+#---------------SG EC2---------------
 resource "aws_security_group" "wp" {
+  name = "${var.name}sg-wp"
   egress {
-      cidr_blocks      = [ "0.0.0.0/0", ]
+      cidr_blocks      = [ "0.0.0.0/0" ]
       description      = ""
       from_port        = 0
       ipv6_cidr_blocks = []
@@ -59,12 +80,24 @@ resource "aws_security_group" "wp" {
       ipv6_cidr_blocks = []
       prefix_list_ids  = []
       self             = false
+   },
+   {
+      cidr_blocks      = [ "0.0.0.0/0" ]
+      description      = "HTTPs connection"
+      from_port        = 443
+      protocol         = "tcp"
+      security_groups  = []
+      to_port          = 443
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      self             = false
    }
    ]
 }
 
+#---------------SG RDS---------------
 resource "aws_security_group" "bbdd_ec2_conncetion" {
- 
+  name = "${var.name}sg-db-connection"
   ingress                = [
     {
       cidr_blocks      = [ "0.0.0.0/0" ]
@@ -78,17 +111,4 @@ resource "aws_security_group" "bbdd_ec2_conncetion" {
       self             = false
    }
    ]
-}
-
-resource "aws_key_pair" "marc" {
-  key_name   = "aws_key_marc"
-  public_key = var.ssh_key
-}
-
-output "IP_EC2" {
-  value= aws_instance.wp.public_ip
-}
-
-output "endpoint_BBDD" {
-  value= aws_db_instance.bbdd.endpoint
 }
